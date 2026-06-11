64b4ed51dc7af44b0c2ae8fc104fa6f9cfc5c577
nurufu <>
2026-06-11 18:35:40 -0400

    added missing ponders to other molds

--- src/main/java/net/zlt/create_modular_tools/item/ModularItems.java
+++ src/main/java/net/zlt/create_modular_tools/item/ModularItems.java
@@ -12,4 +12,34 @@ public class ModularItems {
 	public static final ItemEntry<Item> OAK_MOLD =
 			CreateModularTools.REGISTRATE.item("oak_mold", Item::new)
 					.register();
+	public static final ItemEntry<Item> BIRCH_MOLD =
+			CreateModularTools.REGISTRATE.item("birch_mold", Item::new)
+					.register();
+	public static final ItemEntry<Item> SPRUCE_MOLD =
+			CreateModularTools.REGISTRATE.item("spruce_mold", Item::new)
+					.register();
+	public static final ItemEntry<Item> JUNGLE_MOLD =
+			CreateModularTools.REGISTRATE.item("jungle_mold", Item::new)
+					.register();
+	public static final ItemEntry<Item> ACACIA_MOLD =
+			CreateModularTools.REGISTRATE.item("acacia_mold", Item::new)
+					.register();
+	public static final ItemEntry<Item> DARK_OAK_MOLD =
+			CreateModularTools.REGISTRATE.item("dark_oak_mold", Item::new)
+					.register();
+	public static final ItemEntry<Item> MANGROVE_MOLD =
+			CreateModularTools.REGISTRATE.item("mangrove_mold", Item::new)
+					.register();
+	public static final ItemEntry<Item> CHERRY_MOLD =
+			CreateModularTools.REGISTRATE.item("cherry_mold", Item::new)
+					.register();
+	public static final ItemEntry<Item> BAMBOO_MOLD =
+			CreateModularTools.REGISTRATE.item("bamboo_mold", Item::new)
+					.register();
+	public static final ItemEntry<Item> CRIMSON_MOLD =
+			CreateModularTools.REGISTRATE.item("crimson_mold", Item::new)
+					.register();
+	public static final ItemEntry<Item> WARPED_MOLD =
+			CreateModularTools.REGISTRATE.item("warped_mold", Item::new)
+					.register();
 }
